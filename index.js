bd87d0044e6827c570f474b1c5ad36b5f3a949e7
Dylan Parson <dylan.parson001@gmail.com>
2022-05-30 21:18:38 -0400

    small changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const aboutBtn = document.getElementById("about-button");
 const aboutMe = document.getElementById("about-me");
 const closeButton = document.getElementById("close");
+const main = document.querySelector(".main");
 
 // Listener for adding about me
 aboutBtn.addEventListener("click", () => {
   aboutMe.classList.remove("hidden");
 });
 
-// Listener(s) for closing about me by clicking outside of section
+// Listener for closing about me by clicking outside of section
 document.addEventListener("click", (event) => {
   if (event.target == aboutMe) {
+    console.log('hello')
     aboutMe.classList.add("hidden");
   }
 });
