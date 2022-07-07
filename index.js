bcc93acf067515d361e7c57ffe2e80c20552762f
Dylan Parson <dylan.parson001@gmail.com>
2022-07-06 21:52:17 -0400

    Added SVG for heading and projects background, added skills section

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const aboutBtn = document.getElementById("about-button");
 const aboutMe = document.getElementById("about-me");
 const closeButton = document.getElementById("close");
 const main = document.querySelector(".main");
+const arrow = document.querySelector("#top-arrow")
+
+//Get the button:
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    arrow.classList.remove("hidden");
+  } else {
+    arrow.classList.add("hidden");
+  }
+}
 
 // Listener for adding about me
 aboutBtn.addEventListener("click", () => {
@@ -19,3 +32,22 @@ document.addEventListener("click", (event) => {
 closeButton.addEventListener("click", () => {
   aboutMe.classList.add("hidden");
 });
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+function reveal() {
+  var reveals = document.querySelectorAll(".fade-in");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
