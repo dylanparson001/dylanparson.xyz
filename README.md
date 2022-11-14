d951de4c9029caf09880f3ffae3bf49ca8201dc0
dylan <dylan.parson001@gmail.com>
2022-11-13 23:12:58 -0500

    style changes, got rid of fade in animation and language icons

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Link: https://dylanparson.xyz/
+
+#Dylan Parson Personal Page
+
+My personal website and portfolio
--- index.js
+++ index.js
@@ -35,19 +35,3 @@ closeButton.addEventListener("click", () => {
 
 window.addEventListener("scroll", reveal);
 
-// To check the scroll position on page load
-reveal();
-
-function reveal() {
-  var reveals = document.querySelectorAll(".fade-in");
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
